b8c7a7aec8009491c194282c80993bc47238426a
TheMachine <WhisperingMachine@outlook.com>
2022-01-30 18:04:42 +0800

    test27

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flowlauncher import FlowLauncher
 import re
 from plugin.helper import Actions
 import webbrowser
+import pyperclip
 
 plugindir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(plugindir)
@@ -20,9 +21,9 @@ class QuickerActionSearch(FlowLauncher):
         for item in act_dict.keys():
             if item.find(param) != -1:
                 act_list = act_dict[item]
-                jsonlist.append({"Title": act_list[1], "SubTitle": act_list[2], "IcoPath": "Images/app.png",
+                jsonlist.append({"Title": act_list[1], "SubTitle": act_list[2], "IcoPath": act_list[3],
                                  "JsonRPCAction": {"method": "exec_action", "parameters": [act_list[0]], "scores": 0},
-                                 "ContextData": [act_list[13]]}
+                                 "ContextData": [act_list[13],act_list[0]]}
                                 )
 
         return jsonlist
@@ -31,11 +32,20 @@ class QuickerActionSearch(FlowLauncher):
         return [
             {
                 "Title": "打开动作网页",
-                "SubTitle": "在浏览器中打开动作网页",
+                "SubTitle": data[0],
                 "IcoPath": "Images/app.png",
                 "JsonRPCAction": {
                     "method": "open_url",
                     "parameters": [data[0]]
+                }
+            },
+            {
+                "Title": "复制动作id",
+                "SubTitle": data[1],
+                "IcoPath": "Images/app.png",
+                "JsonRPCAction": {
+                    "method": "open_url",
+                    "parameters": [data[1]]
                 }
             }
         ]
@@ -46,6 +56,10 @@ class QuickerActionSearch(FlowLauncher):
 
     def open_url(self, url):
         webbrowser.open(url)
+
+    def copy_action_id(self,action_id):
+        pyperclip.copy(action_id)
+
 
 
 if __name__ == "__main__":
